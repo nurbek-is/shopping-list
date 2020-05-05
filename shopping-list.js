df0b496befe1adf836b6a9ed9fb06e431521c36a
nurbek82 <nurbek82@gmail.com>
2020-05-05 15:05:37 -0700

    Add files via upload

--- shopping-list.js
+++ shopping-list.js
@@ -1,20 +1,52 @@
 /* Log Messages */
 function log(msg) {
-  // Your code here
+  const log=document.querySelector('section#log>ol')
+  const newItem=document.createElement('li')
+  const now = new Date();
+  newItem.innerHTML = now.toLocaleTimeString() + '<em> '+ msg +' </em>'
+   log.appendChild(newItem)
 }
 
 /* Remove item from list */
 function removeFromList(e) {
-  log('Item Removed');
+const item = e.currentTarget.parentNode;
+item.remove();s
+  log(item.title + 'was Removed');
 }
 
 /* Add product to list */
-function addToList(product) {
-  log(product + ' added.');
+function addToList(product) { 
+  const activelist=document.getElementById('active-items-list');
+  const newItem= document.createElement('li');
+  newItem.title = product;
+  newItem.innerHTML = product + ' ';
+  log(product + ' added.'); 
+  activelist.appendChild(newItem); 
 }
+  
+  
 
 function init() {
   log('Page Loaded');
+  addToList()
+
+  const btnListAdd=document.getElementsByClassName('btn-add');
+    const btnAddNewItem=document.getElementById('add-new-item');
+    const newItem = document.getElementById("new-item");
+    newItem.focus()
+    for(btn of btnListAdd) {
+      btn.addEventListener('click',function(e){
+        const button = e.currentTarget;
+        const product=button.name;
+        addToList(product);
+        newItem.focus();
+      })
+    } 
+     /* Add event listener to New Item Add button */
+     btnAddNewItem.addEventListener('click',function() {
+       addToList(newItem.value)
+       newTextVal='';
+     })
 }
 
 window.addEventListener("load", init);
